9d72f253d2fb6a960cb52c3e84b873dd3c68a006
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-09-07 13:08:53 +0700

    Init Project File

---
+++ src/main/java/int222/project/NpnshopApplication.java
@@ -0,0 +1,13 @@
+package int222.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpnshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpnshopApplication.class, args);
+	}
+
+}

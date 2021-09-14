5676e73ba40befc494b7fa1e8e56126ec64d2c93
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-09-14 23:52:13 +0700

    Add Repositories to check sample data

--- src/main/java/int222/project/controllers/GeneralRestController.java
+++ src/main/java/int222/project/controllers/GeneralRestController.java
@@ -7,7 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import int222.project.models.Brand;
+import int222.project.models.Category;
+import int222.project.models.Color;
 import int222.project.models.Product;
+import int222.project.repositories.BrandJpaRepositories;
+import int222.project.repositories.CategoryJpaRepository;
+import int222.project.repositories.ColorJpaRepositories;
 import int222.project.repositories.ProductJpaRepository;
 
 @RestController
@@ -15,10 +21,31 @@ public class GeneralRestController {
 	
 	@Autowired
 	ProductJpaRepository prodRepo;
+	@Autowired
+	BrandJpaRepositories brandRepo;
+	@Autowired
+	CategoryJpaRepository catRepo;
+	@Autowired
+	ColorJpaRepositories colorRepo;
 	
 	@GetMapping("/health")
 	public String checkHealth() {
 		return "Service is working!!!!";
 	}
 	
+	// For Testing //
+	@GetMapping("/brands")
+	public List<Brand> getBrands() {
+		return brandRepo.findAll();
+	}
+	
+	@GetMapping("/cats")
+	public List<Category> getCategories() {
+		return catRepo.findAll();
+	}
+	
+	@GetMapping("/colors")
+	public List<Color> getColors() {
+		return colorRepo.findAll();
+	}
 }

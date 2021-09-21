d638e8e9f221bf989979eae2b9abaa09ac65f80c
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-09-21 09:46:00 +0700

    Add User Model

---
+++ src/main/java/int222/project/models/User.java
@@ -0,0 +1,44 @@
+package int222.project.models;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer uid;
+	private String username;
+	private String fname;
+	private String lname;
+    @Temporal(TemporalType.DATE)
+    @JsonFormat(pattern = "yyyy-MM-dd")
+	private Date dob;
+    private String address;
+    private String email;
+    private String tel;
+    private Integer usertype;
+    
+}

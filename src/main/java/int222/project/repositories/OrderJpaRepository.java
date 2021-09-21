687d6de44be9141610851b886408fba292ac64b1
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-09-21 13:27:59 +0700

    Change from order to orders

--- src/main/java/int222/project/repositories/OrderJpaRepository.java
+++ src/main/java/int222/project/repositories/OrderJpaRepository.java
@@ -2,8 +2,8 @@ package int222.project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import int222.project.models.Order;
+import int222.project.models.Orders;
 
-public interface OrderJpaRepository extends JpaRepository<Order, Integer> {
+public interface OrderJpaRepository extends JpaRepository<Orders, Integer> {
 
 }

fe8efff73ccc30b648ad48404dc2c6d202d04906
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-09-21 13:28:09 +0700

    Change from User to Users

--- src/main/java/int222/project/repositories/UserJpaRepository.java
+++ src/main/java/int222/project/repositories/UserJpaRepository.java
@@ -2,8 +2,8 @@ package int222.project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import int222.project.models.User;
+import int222.project.models.Users;
 
-public interface UserJpaRepository extends JpaRepository<User, Integer> {
+public interface UserJpaRepository extends JpaRepository<Users, Integer> {
 	
 }

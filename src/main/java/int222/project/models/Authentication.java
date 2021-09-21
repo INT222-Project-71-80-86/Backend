e409ec827c5848c6ddac5f25808fa817b8a5a157
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-09-21 10:38:03 +0700

    Add Auth & User Model. NOTED: Must change properties in the future

---
+++ src/main/java/int222/project/models/Authentication.java
@@ -0,0 +1,38 @@
+package int222.project.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Authentication implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private Integer uid;
+	
+	@OneToOne
+	@MapsId
+	@JoinColumn(name = "uid")
+	@JsonBackReference
+	private User user;
+	
+	private String password;
+	
+}
--- src/main/java/int222/project/models/User.java
+++ src/main/java/int222/project/models/User.java
@@ -3,14 +3,18 @@ package int222.project.models;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,6 +26,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(value = "auth")
 @Entity
 public class User implements Serializable{
 
@@ -40,5 +45,9 @@ public class User implements Serializable{
     private String email;
     private String tel;
     private Integer usertype;
+    
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    @PrimaryKeyJoinColumn
+    private Authentication auth;
     
 }

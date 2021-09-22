e947a88b99226891ea509e34a3e89bf484e96b22
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-09-22 10:52:26 +0700

    Add FindByName Method

--- src/main/java/int222/project/repositories/BrandJpaRepository.java
+++ src/main/java/int222/project/repositories/BrandJpaRepository.java
@@ -1,9 +1,13 @@
 package int222.project.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import int222.project.models.Brand;
 
 public interface BrandJpaRepository extends JpaRepository<Brand, Integer>{
+	
+	List<Brand> findByName(String name);
 	
 }

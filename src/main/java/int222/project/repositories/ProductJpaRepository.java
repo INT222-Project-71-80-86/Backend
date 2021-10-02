b5b7d59fe61f7780510aff047477b662570df5c2
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-10-02 10:52:48 +0700

    Add method query to return rows that contain query value.

--- src/main/java/int222/project/repositories/ProductJpaRepository.java
+++ src/main/java/int222/project/repositories/ProductJpaRepository.java
@@ -1,9 +1,16 @@
 package int222.project.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import int222.project.models.Product;
 
 public interface ProductJpaRepository extends JpaRepository<Product, Integer> {
 
+	@Query("SELECT p from Product p WHERE p.name LIKE %?1% OR p.description LIKE %?1%")
+	public Page<Product> findProductByString(String query, Pageable pageable);
 }

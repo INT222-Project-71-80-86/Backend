5a17dae09f7065167b300cdb9708a446af6fec00
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-10-04 11:43:05 +0700

    Add Method for get reviews of the product

--- src/main/java/int222/project/repositories/ReviewJpaRepository.java
+++ src/main/java/int222/project/repositories/ReviewJpaRepository.java
@@ -1,9 +1,13 @@
 package int222.project.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import int222.project.models.Review;
 
 public interface ReviewJpaRepository extends JpaRepository<Review, Integer> {
+	
+	public List<Review> findReviewByProductPid(Integer pid);
 
 }

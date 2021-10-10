5d78f053df3e644098709f3ed9be9d99f70917c1
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-10-10 19:08:47 +0700

    Add filter + secret from application.properties

--- src/main/java/int222/project/configs/SecurityConfig.java
+++ src/main/java/int222/project/configs/SecurityConfig.java
@@ -11,7 +11,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import int222.project.filter.CustomAuthenticationFilter;
+import int222.project.filter.CustomAuthorizationFilter;
 import int222.project.services.MyUserServices;
 import lombok.RequiredArgsConstructor;
 
@@ -28,10 +31,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
 		http.csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(STATELESS);
 		http.authorizeRequests().antMatchers("/api/*").permitAll();
 		http.authorizeRequests().anyRequest().authenticated();
+		http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 		
 	}
 	

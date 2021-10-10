29b0c1098ebbe177c285833526f157c683a6b297
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-10-10 12:50:21 +0700

    Add Query method with deleted flags related

--- src/main/java/int222/project/repositories/ColorJpaRepository.java
+++ src/main/java/int222/project/repositories/ColorJpaRepository.java
@@ -3,6 +3,7 @@ package int222.project.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import int222.project.models.Color;
 
@@ -11,6 +12,13 @@ public interface ColorJpaRepository extends JpaRepository<Color, Integer>{
 //	public List<Color> findByName(String name);
 //	public List<Color> findByCode(String code);
 	
+	@Query("SELECT c FROM Color c WHERE (c.name = ?1 OR c.code = ?2) AND c.deleted = 0")
 	public List<Color> findByNameOrCode(String name, String code);
+	
+	@Query("SELECT c FROM Color c WHERE (c.name = ?1 OR c.code = ?2) AND c.cid != ?3 AND c.deleted = 0")
+	public List<Color> findByOtherNameOrCode(String name, String code, Integer cid);
+	
+	@Query("SELECT c FROM Color c WHERE c.deleted = 0")
+	public List<Color> findAllColors();
 	
 }

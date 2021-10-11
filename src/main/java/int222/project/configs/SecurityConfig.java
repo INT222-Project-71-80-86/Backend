36a5475906ed6d4d4054793e0b17095cccfeacb9
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-10-11 21:07:24 +0700

    Add authorizeRequest To security config

--- src/main/java/int222/project/configs/SecurityConfig.java
+++ src/main/java/int222/project/configs/SecurityConfig.java
@@ -2,8 +2,10 @@ package int222.project.configs;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -23,6 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	private final MyUserServices userService;
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+	@Value("${jwt.token.secret}") private String secret;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -31,15 +34,33 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+		CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean(),secret);
         customAuthenticationFilter.setFilterProcessesUrl("/api/login");
 		http.csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(STATELESS);
-		http.authorizeRequests().antMatchers("/api/*").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/users", "/api/file").hasAnyAuthority("ROLE_ADMIN");
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/product/**", "/api/product", "/api/brand", "/api/brand/*",
+				"/api/color", "/api/color/*", "/api/cats", "/api/file/*", "/api/review/*").permitAll();
+		/* ----------------------POST-------------------- */
+		http.authorizeRequests().antMatchers(HttpMethod.POST,  "/api/user/save").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/brand/save", "/api/color/save").hasAnyAuthority("ROLE_ADMIN");
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/product/save").hasAnyAuthority("ROLE_STAFF","ROLE_ADMIN");
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/something").hasAnyAuthority("ROLE_STAFF");
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/review/save").hasAnyAuthority("ROLE_CUSTOMER");
+		/* ----------------------PUT--------------------- */
+		http.authorizeRequests().antMatchers(HttpMethod.PUT, "/api/brand/edit", "/api/color/edit").hasAnyAuthority("ROLE_ADMIN");
+		http.authorizeRequests().antMatchers(HttpMethod.PUT, "/api/product/edit").hasAnyAuthority("ROLE_STAFF","ROLE_ADMIN");
+		http.authorizeRequests().antMatchers(HttpMethod.PUT, "/api/something").hasAnyAuthority("ROLE_STAFF");
+		http.authorizeRequests().antMatchers(HttpMethod.PUT, "/api/review/edit").hasAnyAuthority("ROLE_CUSTOMER");
+		/* ---------------------DELETE------------------- */
+		http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/api/brand/delete/*", "/api/color/delete/*").hasAnyAuthority("ROLE_ADMIN");
+		http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/api/product/delete/*").hasAnyAuthority("ROLE_STAFF","ROLE_ADMIN");
+		http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/api/something").hasAnyAuthority("ROLE_STAFF");
+		http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/api/something").hasAnyAuthority("ROLE_CUSTOMER");
+		http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/api/review/delete/*").hasAnyAuthority("ROLE_CUSTOMER","ROLE_STAFF","ROLE_ADMIN");
 		http.authorizeRequests().anyRequest().authenticated();
 		http.addFilter(customAuthenticationFilter);
-        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
-		
+        http.addFilterBefore(new CustomAuthorizationFilter(secret), UsernamePasswordAuthenticationFilter.class);
 	}
 	
     @Bean

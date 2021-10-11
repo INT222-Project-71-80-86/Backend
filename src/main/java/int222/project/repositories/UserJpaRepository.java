a72bb79885a44b796dcb34127b78518e8d3ebe70
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-10-12 01:03:10 +0700

    Add new query

--- src/main/java/int222/project/repositories/UserJpaRepository.java
+++ src/main/java/int222/project/repositories/UserJpaRepository.java
@@ -1,11 +1,15 @@
 package int222.project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import int222.project.models.Users;
 
 public interface UserJpaRepository extends JpaRepository<Users, Integer> {
 	
 	Users findByUsername(String username);
+	
+	@Query("SELECT u FROM Users u WHERE u.username = ?1 AND u.uid != ?2")
+	Users findByOtherUsername(String username, Integer uid);
 	
 }

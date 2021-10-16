2b803ff06d9a16e200151b1ebede01aa7c6d3f95
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-10-16 14:08:13 +0700

    Modify Add and Edit review methods

--- src/main/java/int222/project/services/ReviewService.java
+++ src/main/java/int222/project/services/ReviewService.java
@@ -1,5 +1,6 @@
 package int222.project.services;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +15,11 @@ import int222.project.models.Brand;
 import int222.project.models.Product;
 import int222.project.models.Review;
 import int222.project.models.ReviewPK;
+import int222.project.models.Users;
 import int222.project.repositories.BrandJpaRepository;
 import int222.project.repositories.ProductJpaRepository;
 import int222.project.repositories.ReviewJpaRepository;
+import int222.project.repositories.UserJpaRepository;
 
 @Service
 public class ReviewService {
@@ -27,6 +30,8 @@ public class ReviewService {
 	private ReviewJpaRepository reviewRepo;
 	@Autowired
 	private ProductJpaRepository prodRepo;
+	@Autowired
+	private UserJpaRepository userRepo;
 
 	// Methods //
 	// Search all review for a product
@@ -36,12 +41,31 @@ public class ReviewService {
 
 	// Add New review of a product
 	public Review addReviewofProduct(Review r) {
+		Users user = userRepo.findByUsername(r.getUser().getUsername());
+		Product product = prodRepo.findById(r.getId().getPid()).orElseThrow(() -> 
+			new DataRelatedException(ERROR_CODE.PRODUCT_DOESNT_FOUND, "Cannot found this product on database."));
+		if (user == null) {
+			throw new DataRelatedException(ERROR_CODE.USER_DOESNT_FOUND, "Cannot found this user in database.");
+		} else {
+			r.setUser(user);
+			r.setProduct(product);
+			r.setDatetime(new Date());
+			r.setId(new ReviewPK(user.getUid(), r.getId().getPid()));
+		}
 		validateReview(r, false);
 		return reviewRepo.saveAndFlush(r);
 	}
 	
 	// Edit review of a product
 	public Review editReviewOfProduct(Review r) {
+		Users user = userRepo.findById(r.getId().getUid()).orElseThrow(() -> 
+			new DataRelatedException(ERROR_CODE.USER_DOESNT_FOUND, "Cannot found this user on database."));
+		Product product = prodRepo.findById(r.getId().getPid()).orElseThrow(() -> 
+			new DataRelatedException(ERROR_CODE.PRODUCT_DOESNT_FOUND, "Cannot found this product on database."));
+		r.setUser(user);
+		r.setProduct(product);
+		r.setDatetime(new Date());
+			
 		validateReview(r, true);
 		return reviewRepo.saveAndFlush(r);
 	}
@@ -55,15 +79,16 @@ public class ReviewService {
 	}
 	
 	public boolean validateReview(Review r, boolean isEdit) {
+		
 		if (isEdit) {
 			reviewRepo.findById(r.getId()).orElseThrow(() -> new DataRelatedException(ERROR_CODE.ITEM_DOES_NOT_EXIST, 
 					"Can't find review of product: "+r.getId().getPid()+" with user id: "+r.getId().getUid()));
 		}
-		if(r.getProduct().equals(null)) {
+		if(r.getId().getPid().equals(null)) {
 			throw new DataRelatedException(ERROR_CODE.INVALID_ATTRIBUTE, "This review doesn't contain product.");
 		}
-		prodRepo.findById(r.getProduct().getPid()).orElseThrow(() -> new DataRelatedException(ERROR_CODE.PRODUCT_DOESNT_FOUND, 
-				"Can't add review of product with product id: "+r.getProduct().getPid()+". Product doesn't found."));
+		prodRepo.findById(r.getId().getPid()).orElseThrow(() -> new DataRelatedException(ERROR_CODE.PRODUCT_DOESNT_FOUND, 
+				"Can't add review of product with product id: "+r.getId().getPid()+". Product doesn't found."));
 		if(r.getRating() < 0 || r.getRating() >= 6) {
 			throw new DataRelatedException(ERROR_CODE.INVALID_ATTRIBUTE, "Wrong Rating values, should be Integer within range 0-5");
 		}

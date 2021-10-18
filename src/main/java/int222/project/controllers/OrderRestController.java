1958e32fa650a678257ed45d38db095e6e685ee0
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-10-18 13:52:03 +0700

    Change Mapping path, add new method

--- src/main/java/int222/project/controllers/OrderRestController.java
+++ src/main/java/int222/project/controllers/OrderRestController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +19,8 @@ public class OrderRestController {
 	@Autowired
 	private OrderService orderService;
 	
-	@GetMapping("/get/{oid}")
-	public Orders getOrderById(@PathVariable int oid) {
+	@GetMapping("/get/id/{oid}")
+	public Orders getOrderById(@PathVariable int oid, @RequestAttribute String username) {
 		return orderService.getOrderById(oid);
 	}
 	
@@ -27,6 +28,12 @@ public class OrderRestController {
 	public Page<Orders> getOrderPaging(@RequestParam(defaultValue = "0") int pageNo, 
 			@RequestParam(defaultValue = "10") int size, @RequestParam(defaultValue = "oid") String sortBy){
 		return orderService.getAllOrders(pageNo, size, sortBy);
+	}
+	
+	@GetMapping("/get/username")
+	public Page<Orders> getOrderByUsername( @RequestAttribute String username, @RequestParam(defaultValue = "0") int pageNo, 
+			@RequestParam(defaultValue = "10") int size, @RequestParam(defaultValue = "oid") String sortBy){
+		return orderService.getOrdersByUsername(username, pageNo, size, sortBy);
 	}
 	
 	

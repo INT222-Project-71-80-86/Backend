0c49b4cc97b9eda37f3141539c7feba5fb3b27e5
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-10-18 13:51:39 +0700

    Add find order of a user method (using username)

--- src/main/java/int222/project/repositories/OrderJpaRepository.java
+++ src/main/java/int222/project/repositories/OrderJpaRepository.java
@@ -1,9 +1,16 @@
 package int222.project.repositories;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import int222.project.models.Orders;
 
 public interface OrderJpaRepository extends JpaRepository<Orders, Integer> {
+	
+	@Query("SELECT o FROM Orders o WHERE o.user.username = ?1")
+	public Page<Orders> findByUsername(String username, Pageable pageable);
 
 }

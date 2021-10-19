4c9b5276eebbd4bbb433dd2459aca976012a67a3
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-10-19 14:15:07 +0700

    Change general controller

--- src/main/java/int222/project/controllers/GeneralRestController.java
+++ src/main/java/int222/project/controllers/GeneralRestController.java
@@ -1,28 +1,13 @@
 package int222.project.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
-import int222.project.exceptions.DataRelatedException;
-import int222.project.exceptions.ExceptionResponse.ERROR_CODE;
-import int222.project.files.FileInfo;
 import int222.project.models.*;
 import int222.project.repositories.*;
-import int222.project.services.FileStoreServiceImp;
 import int222.project.services.FileStoreServices;
 
 @RestController
@@ -51,25 +36,20 @@ public class GeneralRestController {
 	@Autowired
 	FileStoreServices file;
 	
-	@GetMapping("/health")
+	@GetMapping("/api/health")
 	public String checkHealth() {
 		return "Service is working!!!!";
 	}
 	// Current Working //
 
 	
-	// For Testing //
+	// For Admin //
 	
 	@GetMapping("/api/cats")
 	public List<Category> getCategories() {
 		return catRepo.findAll();
 	}
 	
-//	@GetMapping("/color")
-//	public List<Color> getColors() {
-//		return colorRepo.findAll();
-//	}
-
 	@GetMapping("/api/coupons")
 	public List<Coupon> getCoupon() {
 		return couRepo.findAll();

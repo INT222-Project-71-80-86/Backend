618ba44adb55aa689adc61ad0fe81eb6d853e3c6
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-11-20 19:50:26 +0700

    Change token setting

--- src/main/java/int222/project/filter/CustomAuthenticationFilter.java
+++ src/main/java/int222/project/filter/CustomAuthenticationFilter.java
@@ -69,13 +69,13 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         Algorithm algorithm = Algorithm.HMAC256(secret.getBytes());
         String access_token = JWT.create()
                 .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() +  30 * 60 * 1000)) //30 * 60 * 1000
+                .withExpiresAt(new Date(System.currentTimeMillis() + 6 * 60 * 60 * 1000)) //30 * 60 * 1000
                 .withIssuer(request.getRequestURL().toString())
                 .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
                 .sign(algorithm);
         String refresh_token = JWT.create()
                 .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 12 * 60 * 60 * 1000)) //12 * 60 * 60 * 1000
+                .withExpiresAt(new Date(System.currentTimeMillis() + 3 * 24 * 60 * 60 * 1000)) //12 * 60 * 60 * 1000
                 .withIssuer(request.getRequestURL().toString())
                 .sign(algorithm);
         /*response.setHeader("access_token", access_token);
